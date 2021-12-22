877e2709968ae14c52463778e188c75149c3ec29
Rodrigo Alba <rodrigo@megabadvisors.com>
2021-12-22 11:28:03 -0400

    commit tree 05e789a1840a42568dc4f950edf48721fbbb2abb

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -7,17 +7,18 @@ class ToysController < ApplicationController
   end
 
   def create
-    toy = Toys.create(toy_params)
+    toy = Toy.create(toy_params)
     render json: toy, status: :created
   end
 
   def update
-    toy = Toy.find_by(id: params[:id])
+    toy = find_toy
     toy.update(toy_params)
+    render json: toy, status: :ok
   end
 
   def destroy
-    toy = Toy.find_by(id: params[:id])
+    toy = find_toy
     toy.destroy
     head :no_content
   end
@@ -27,5 +28,10 @@ class ToysController < ApplicationController
   def toy_params
     params.permit(:name, :image, :likes)
   end
+
+  def find_toy
+    Toy.find_by(id: params[:id])
+  end
+  
 
 end
